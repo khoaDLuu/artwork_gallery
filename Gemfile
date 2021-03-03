e0bca605e57b371c1b1d70f0075f7f17987b3a89
Keith Dluu <dangkhoa240899@gmail.com>
2021-03-03 14:13:38 +0700

    --amend

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
+gem 'faker'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
@@ -41,7 +43,6 @@ end
 
 group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
-  gem 'faker'
   gem 'web-console', '>= 3.3.0'
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,46 +1,58 @@
 require 'faker'
 
+case Rails.env
+when "development"
+  # DUMMY USERS
+  puts "Seeding USERS table..."
+  puts "======================"
 
-# DUMMY USERS
-puts "Seeding USERS table..."
-puts "======================"
-
-users = [
-  ['admin0', true],
-  ['admin1', true],
-  ['admin2', true],
-  ['member', false],
-  ['member1', false],
-  ['member2', false]
-]
-# default users
-users.each do |name, is_admin|
+  users = [
+    ['admin0', true],
+    ['admin1', true],
+    ['admin2', true],
+    ['member', false],
+    ['member1', false],
+    ['member2', false]
+  ]
+  # default users
+  users.each do |name, is_admin|
+    user = User.new
+    user.username = name
+    user.password = name
+    user.password_confirmation = name
+    user.email = "#{name}@dummymail.com"
+    user.point = Faker::Number.within(range: 100..1000)
+    user.is_admin = is_admin
+    user.is_banned = false
+    user.credit_card = Faker::PhoneNumber.cell_phone_in_e164[1..]
+    user.save!
+  end
+  # more users
+  40.times do
+    name = Faker::Name.unique.name.gsub! /\s/, '_'
+    user = User.new
+    user.username = name
+    user.password = name
+    user.password_confirmation = name
+    user.email = "#{name}@dummymail.com"
+    user.point = Faker::Number.within(range: 100..1000)
+    user.is_admin = false
+    user.is_banned = Faker::Boolean.boolean(true_ratio: 0.1)
+    user.credit_card = Faker::PhoneNumber.cell_phone_in_e164[1..]
+    user.save!
+  end
+when "production"
   user = User.new
-  user.username = name
-  user.password = name
-  user.password_confirmation = name
-  user.email = "#{name}@dummymail.com"
+  user.username = ENV["ADMIN_ACCOUNT"]
+  user.password = ENV["ADMIN_PASSWORD"]
+  user.password_confirmation = ENV["ADMIN_PASSWORD"]
+  user.email = ENV["ADMIN_EMAIL"]
   user.point = Faker::Number.within(range: 100..1000)
   user.is_admin = is_admin
   user.is_banned = false
-  user.credit_card = Faker::PhoneNumber.cell_phone_in_e164[1..]
+  user.credit_card = ENV["ADMIN_CREDIT_CARD"]
   user.save!
 end
-# more users
-40.times do
-  name = Faker::Name.unique.name.gsub! /\s/, '_'
-  user = User.new
-  user.username = name
-  user.password = name
-  user.password_confirmation = name
-  user.email = "#{name}@dummymail.com"
-  user.point = Faker::Number.within(range: 100..1000)
-  user.is_admin = false
-  user.is_banned = Faker::Boolean.boolean(true_ratio: 0.1)
-  user.credit_card = Faker::PhoneNumber.cell_phone_in_e164[1..]
-  user.save!
-end
-
 
 # CATEGORIES
 puts "Seeding CATEGORIES table..."
